74f5e649bde84b784276f628df4d63b164de7cf9
Crowdbotics <team@crowdbotics.com>
2021-08-25 16:51:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "mute_haze_30099Identifier",
+  url: "https://mute_haze_30099Identifier.botics.co",
+  api: "https://mute_haze_30099Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
